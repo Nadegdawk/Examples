b6c6b640730952433a57ddff1cdb46b3bdae1e23
Nadegdawk <nadegdawk@mail.ru>
2022-12-21 11:44:49 +0300

    add lec4

---
+++ Lection4_Metods/Program.cs
@@ -0,0 +1,145 @@
+﻿// Вид 1 
+void Method1()
+{
+    System.Console.WriteLine("Автор ...");
+}
+// Method1();     //вариант использования
+
+
+// Вид 2
+void Method2(string msg)
+{
+    Console.WriteLine(msg);
+}
+
+// Method2(msg: "Текст сообщения");      //вариант использования
+
+void Method21(string msg, int count)
+{
+    int i = 0;
+    while (i<count)
+    {
+        Console.WriteLine(msg);
+        i++;
+    }
+    
+}
+
+// Method21("Текст сообщения", 4);     //вариант использования
+// Method21(msg: "Текст сообщения", count: 4);     //вариант использования
+// Method21(count: 4, msg: "Текст сообщения");     //вариант использования
+
+// Вид 3
+
+int Method3()
+{
+    return DateTime.Now.Year;  //текущий год
+}
+
+// int year = Method3();     //вариант использования
+// Console.WriteLine(year);
+
+// Вид 4
+
+// string Method4(int count, string text)
+// {
+//     int i = 0;
+//     string result = String.Empty; // пустая строка
+
+//     while (i < count)
+//     {
+//         result = result + text;
+//         i++;
+//     }
+//     return result;
+// }
+
+string Method4(int count, string text)          //альтернатива for
+{
+        string result = String.Empty; // пустая строка
+    for (int i = 0; i < count; i++)
+    {
+        result = result + text;
+    }
+    return result;
+}
+
+// string res = Method4(4, "qwer ");
+// Console.WriteLine(res);
+
+// for (int i = 2; i <= 10; i++)
+// {
+//     for (int j = 2; j <= 10; j++)
+//     {
+//         Console.WriteLine($"{i} x {j} = {i*j}");
+//     }
+//     Console.WriteLine();
+// }
+
+// Дан текст. В тексте нужно все пробелы заменить черточками,
+// маленькие буквы “к” заменить большими “К”, а большие “С” заменить
+// маленькими “с”.
+
+string text = "— Я думаю, — сказал князь, улыбаясь, — что, "
++ "ежели бы вас послали вместо нашего милого "
++ "Винценгероде, вы бы взяли приступом согласие "
++ "прусского короля. Вы так красноречивы. Вы дадите мне чаю?";
+
+//string s = "qwerty"
+//            012345
+//s[3] // r             // обращение к элементу строки
+
+string Replace(string text, char oldValue, char newValue)
+{
+    string result = string.Empty;
+    int length = text.Length;
+    for (int i = 0; i < length; i++)
+    {
+        if (text[i] == oldValue) result = result + $"{newValue}";
+        else result = result + $"{text[i]}";
+    }
+    return result;  
+}
+// string newText = Replace(text, ' ', '|');
+// Console.WriteLine(newText);
+// Console.WriteLine();
+// newText = Replace(newText, 'к', 'К');
+// Console.WriteLine(newText);
+// Console.WriteLine();
+// newText = Replace(newText, 'с', 'С');
+// Console.WriteLine(newText);
+
+
+//Массивы
+int[] arr = { 1, 5, 4, 3, 2, 6, 7, 1, 1 };
+
+void PrintArray(int[] array)        //вывод массива
+{
+    int count = array.Length;
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+// PrintArray(arr);
+
+void SelectionSort(int[] array)       // метод сортировки массива
+{
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        int minPosition = i;
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if (array[j] < array[minPosition]) minPosition = j;
+        }
+        int temporary = array[i];
+        array[i] = array[minPosition];
+        array[minPosition] = temporary;
+    }
+}
+
+PrintArray(arr); //вывод массива
+SelectionSort(arr); //сортировка массива
+PrintArray(arr);//вывод отсортированного массива
